3dc4cdb69817b6d046a4b4044b2b920049635176
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-14 13:54:24 -0700

    Modified index to work with JSX

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StrictMode} from 'react'; //TODO remove for prod
+import { BrowserRouter, Routes, Route } from 'react-router';
+import ReactDOM from 'react-dom/client';
+// import { Toaster } from "react-hot-toast";
+/* Specific page imports*/
+import ATPage from "./pages/AppalachianTrailPage.jsx"
+import HomePage from "./pages/HomePage.jsx";
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <StrictMode>
+      {/* <Toaster position="top-right" /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/AT" element={<ATPage />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
+);
