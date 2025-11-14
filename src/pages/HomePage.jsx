a7bc93f00886d18605341b940ba4c994e3d5390a
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-14 13:54:35 -0700

    Added Homepage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+import Navbar from '../componants/WebsiteNavbar.jsx'
+import React from 'react';
+
+export default function HomePage() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
