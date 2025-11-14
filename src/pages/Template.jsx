9bab5c49e50394a2cead08f54bbb054e304941d1
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-14 13:52:36 -0700

    Created Helper template to save time making new pages

---
+++ src/pages/Template.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../componants/WebsiteNavbar.jsx'
+import React from 'react';
+
+/* This template just exists to make my life easier when making new pages*/
+export default function Template() {
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
