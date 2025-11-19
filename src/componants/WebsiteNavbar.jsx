0d524462855004416dd09008952432f675be9f92
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-19 15:40:55 -0700

    Added photos in public folder

--- src/componants/WebsiteNavbar.jsx
+++ src/componants/WebsiteNavbar.jsx
@@ -5,17 +5,18 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Navbar from "react-bootstrap/Navbar";
 
 export default function WebsiteNavbar() {
+    let base = "#"
     return (
       <Navbar collapseOnSelect expand="sm" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand to="/">Jen Gordon</Navbar.Brand>
+          <Navbar.Brand href={base + ""}>Jen Gordon</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link to="/AT">Appalachian Trail</Nav.Link>
-              <Nav.Link to="/projects">Projects</Nav.Link>
+              <Nav.Link href={base + "AT"}>Appalachian Trail</Nav.Link>
+              <Nav.Link href={base + "projects"}>Projects</Nav.Link>
               <NavDropdown title="Posts" id="collapsible-nav-dropdown">
-                <NavDropdown.Item to="">TidePool</NavDropdown.Item>
+                <NavDropdown.Item href={base + ""}>TidePool</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
