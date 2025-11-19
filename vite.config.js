d34751e3b00dc00576dbdb67571d7e5fe914155f
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-19 10:24:38 -0700

    Added needed vite modules

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import {defaultConfig, defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+
+export default defineConfig({
+    plugins: [react()],
+    base: "/Personal-Website"
+});
