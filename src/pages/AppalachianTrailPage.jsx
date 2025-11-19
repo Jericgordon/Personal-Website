fa0b312c7d0ce178f4f74b799057f11d7b358174
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-19 16:10:11 -0700

    updated data pages so that differences in it are deployed

--- src/pages/AppalachianTrailPage.jsx
+++ src/pages/AppalachianTrailPage.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../componants/WebsiteNavbar.jsx'
 import React from 'react';
 import Map from '../componants/Map.jsx';
-import data from "../../helper/export.json" with {type: "json"}
+import data from "../../public/export.json" with{type: "json"}
 
 export default function HomePage() {
     return (
