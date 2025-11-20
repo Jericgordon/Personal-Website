d22c9e6291124b3b0f7345f0eadd12d51e4ae793
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-20 16:40:04 -0700

    Added Project page with better looking cards

---
+++ src/componants/ProjectCard.jsx
@@ -0,0 +1,25 @@
+import github_image from "../icons/github-mark/github-mark-white.png"
+import rightArrow from "../icons/google-icons/arrow_forward_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png"
+import React from "react";
+import { Button } from "react-bootstrap";
+import "../css/ProjectCard.css"
+
+export default function ProjectCard ({title,description,ghLink,viewLink}){
+    console.log("loading card", title);
+    return( 
+        <div className="card-style">
+            <h3>{title}</h3>
+            <div className="card-description">
+                {description}
+            </div>
+            <Button size="sm" href={ghLink} variant="dark" className="github-button">
+                <img height={15} width={15} src={github_image}></img>
+            </Button>
+
+            <Button size="sm" href={viewLink} variant="success" className="view-button">
+                <img height={15} width={15} src={rightArrow}></img>
+                view
+            </Button>
+        </div>
+    );
+}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,15 +1,25 @@
-import FlipCard from '../componants/FlipCard.jsx';
+import ProjectCard from '../componants/ProjectCard.jsx';
 import Navbar from '../componants/WebsiteNavbar.jsx'
 import React from 'react';
+import "../css/Projects.css"
+import projectData from "../data/projects.json" with {type : "json"}
+
+// projectData.map((d) => 
+// {
+//     console.log(d.viewLink,d.title,d.description,d.githubLink,d.viewLink)
+// })
+
 
 /* This template just exists to make my life easier when making new pages*/
 export default function Projects() {
     return (
         <>
             <Navbar />
-            <FlipCard/>
-            
+            <div className='projects'>
+                {projectData.map((data) => {
+                    return <ProjectCard key={data.title} title={data.title} description={data.description} ghLink={data.githubLink} viewLink={data.viewLink}  />
+                })}
+            </div>
         </>
     )
-    
 }
