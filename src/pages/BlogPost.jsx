14e4cc8a69b819c295e2627e9d720161ca966a1f
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2025-11-21 16:21:06 -0700

    Added HTML to blogposts

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import Navbar from '../componants/WebsiteNavbar.jsx'
 import { useParams } from 'react-router-dom';
-
+import blogPosts from "../data/blogPosts.json" with {type : "json"}
 
 export default function BlogPost() {  
     const { id } = useParams();
     console.log("ID is", id);
+    
 
     return (
         <>
             <Navbar />
+            <div dangerouslySetInnerHTML={{__html: blogPosts[parseInt(id)]["html"]}}>
+
+            </div>
             
+
         </>
     )
 }
