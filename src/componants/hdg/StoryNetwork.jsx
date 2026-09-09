b03846116a69c41c5ba1b727941f8a98c8881d9e
Jen Gordon <jericgordon@gmail.com>
2026-09-08 18:56:18 -0600

    taking out datazoom

--- src/componants/hdg/StoryNetwork.jsx
+++ src/componants/hdg/StoryNetwork.jsx
@@ -98,14 +98,13 @@ class StoryNetwork extends React.Component {
     getOption = () => {
         const options =  {
             tooltip: {},
-            dataZoom: [
-            {
-                type: 'inside', // Enables touch gestures like pinch & pan on mobile
-                zoomOnMouseWheel: true,
-                moveOnMouseMove: true,
-                xAxisIndex: 0,   // Restrict gesture zooming to the X-axis only
-                preventDefaultMouseMove: true, // Prevents mobile web views from scrolling while dragging the graph
-            }],
+            // dataZoom: [
+            // {
+            //     type: 'inside', // Enables touch gestures like pinch & pan on mobile
+            //     zoomOnMouseWheel: true,
+            //     moveOnMouseMove: true,
+            //     xAxisIndex: 0,   // Restrict gesture zooming to the X-axis only
+            // }],
             color:["#103783", "#2C4F94", "#4867A5", "#637FB7", "#7F97C8", "#9BAFD9"],
             legend: [{
                 data: this.years,
